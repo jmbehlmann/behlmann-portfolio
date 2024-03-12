31f8e5eac3b2b080bf698512099f10a38bea10ac
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-12 16:22:36 -0500

    created projects component

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { Links } from "./components/Links.jsx";
+import { Projects } from "./components/Projects.jsx";
 
 export function Content() {
   return (
     <div>
       <Header />
       <Links />
+      <Projects />
       <Footer />
     </div>
   )
