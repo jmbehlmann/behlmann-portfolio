d4d6dd743ae4858aae6ed1c26c00827a5f7612a2
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-12 12:07:47 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header.jsx";
+import { Footer } from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
