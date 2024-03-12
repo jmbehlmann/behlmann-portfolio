4ddc5596e776dd2ec4e9ff295eaa897c5b6399da
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-12 16:36:48 -0500

    created about component

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,6 +2,8 @@ import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { Links } from "./components/Links.jsx";
 import { Projects } from "./components/Projects.jsx";
+import { About } from "./components/About.jsx";
+import { Experience } from "./components/Experience.jsx";
 
 export function Content() {
   return (
@@ -9,6 +11,8 @@ export function Content() {
       <Header />
       <Links />
       <Projects />
+      <About />
+      <Experience />
       <Footer />
     </div>
   )
