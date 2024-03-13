dbbdbe5d29914ba5ad7118edfee4adb3d16f8451
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-13 15:55:06 -0500

    style projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,13 +2,20 @@ export function Projects() {
   return (
     <div>
       <h2>Projects</h2>
-      <ul>
-        <li>
+      <div>
+        <div className="card p-2">
           <a className="link" href="https://jmbehlmann.github.io/bike-rack-chicago-frontend/" target="_blank">Bike Rack Chicago</a>
-        </li>
-        <li>Portfolio site</li>
-        <li>Rising Ash Designs</li>
-      </ul>
+          <ul>
+            <li>Bike Rack Chicago is a web app that helps users find bike racks in Chicago by address, place, or current location. It uses Google Maps and React for an interactive map, Google Maps APIs for address autocompletion, and data from the City of Chicago. The Rails and PostgreSQL backend is deployed on Heroku while the frontend is on Github Pages.</li>
+          </ul>
+        </div>
+        <div className="card p-2">
+          <a className="link" href="#" target="_blank">Portfolio Site</a>
+        </div>
+        <div className="card p-2">
+          <a className="link" href="https://www.risingashdesigns.com/" target="_blank">Rising Ash Designs</a>
+        </div>
+      </div>
     </div>
   )
 }
