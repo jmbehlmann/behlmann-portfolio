2681754ac628e1b66c40adf6f833f936735f6a2b
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-20 17:23:05 -0500

    added resume link

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,12 @@ export function Experience() {
   return (
     <div>
       <h2>Experience</h2>
+      <a
+        href="/Behlmann_resume.pdf"
+        download
+      >
+        Download Resume
+      </a>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ante at quam tristique venenatis. Nunc laoreet quam sit amet odio convallis, id varius nisl condimentum. Donec aliquet nunc purus, vel congue quam accumsan quis. Quisque ut ultricies nisl, nec ultrices ipsum. Nam eget quam quis odio elementum condimentum. Nunc commodo lacus vel viverra faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus sodales eget nisi at eleifend. Pellentesque odio augue, ullamcorper sit amet dolor at, gravida venenatis augue. Nunc ex mi, consequat at urna et, molestie vestibulum nibh. Etiam eu nisl a tortor pretium sagittis non a libero. Nunc gravida semper ante sit amet dignissim.
 
         Vivamus viverra magna sed venenatis dignissim. Donec sagittis nulla tortor, eget suscipit nisi finibus ac. Donec pharetra, tellus sit amet laoreet blandit, augue felis molestie purus, in luctus risus tellus in nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ut lacus vel tellus ullamcorper sagittis. Aliquam erat volutpat. Proin eu fringilla mauris. Ut ullamcorper dui a quam maximus, non luctus urna dictum. Proin dignissim metus ac nunc gravida facilisis.
