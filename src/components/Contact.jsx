b81dda8cb100f9799420134ff5073d62a6b5086a
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-20 18:11:24 -0500

    basic contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,14 +2,69 @@ export function Contact() {
   return (
     <div>
       <h2>Contact</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ante at quam tristique venenatis. Nunc laoreet quam sit amet odio convallis, id varius nisl condimentum. Donec aliquet nunc purus, vel congue quam accumsan quis. Quisque ut ultricies nisl, nec ultrices ipsum. Nam eget quam quis odio elementum condimentum. Nunc commodo lacus vel viverra faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus sodales eget nisi at eleifend. Pellentesque odio augue, ullamcorper sit amet dolor at, gravida venenatis augue. Nunc ex mi, consequat at urna et, molestie vestibulum nibh. Etiam eu nisl a tortor pretium sagittis non a libero. Nunc gravida semper ante sit amet dignissim.
-
-
-        Aenean mi sapien, viverra vitae varius sit amet, consectetur sit amet tortor. Fusce fermentum mi eget nisl mattis vulputate. Vestibulum elementum erat nisl, quis porttitor elit fringilla eu. Pellentesque ullamcorper lacus ut purus vestibulum, eget auctor ligula iaculis. Donec bibendum, augue a convallis pellentesque, est diam mollis leo, eu dapibus mi justo nec purus. Praesent ultrices diam erat, vitae suscipit velit feugiat in. Ut sit amet odio vitae massa interdum convallis nec et tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Etiam gravida erat nisi, id maximus nisi vehicula rutrum. Donec sed ante quis urna mattis placerat in sed quam. Sed venenatis neque et eros tristique lobortis. Nullam ut ligula a nunc blandit congue sit amet et dui. Ut maximus, nisl nec faucibus posuere, nisl mauris consectetur nisl, vitae euismod massa velit sed urna.
-
-        Nunc et feugiat sem, porttitor placerat erat. Integer ut porta sem, sed condimentum nulla. Cras sed enim in diam faucibus suscipit. Maecenas accumsan tristique posuere. Vivamus pretium dui nec tempus egestas. Pellentesque eleifend nibh quam, in interdum mauris sagittis at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus dapibus augue ac mauris faucibus, vel feugiat lorem viverra. Nunc mauris risus, dapibus in turpis et, auctor mollis turpis. Suspendisse elementum pharetra turpis, eget rutrum nulla. Donec arcu nunc, placerat in ante at, ultrices congue urna. Aliquam tincidunt consequat magna, eu accumsan felis pharetra et. Nunc ut enim et turpis tempus tincidunt.
-
-      </p>
+      <div className='ContactForm'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='contactForm'>
+                <form id='contact-form' noValidate>
+                  {/* Row 1 of form */}
+                  <div className='row py-2 formRow'>
+                    <div className='col'>
+                      <input
+                        type='text'
+                        name='name'
+                        className='form-control formInput'
+                        placeholder='Name'
+                      ></input>
+                    </div>
+                  </div>
+                  {/* Row 2 of form */}
+                  <div className='row py-2 formRow'>
+                    <div className='col'>
+                      <input
+                        type='email'
+                        name='email'
+                        className='form-control formInput'
+                        placeholder='Email address'
+                      ></input>
+                    </div>
+                  </div>
+                  {/* Row 3 of form */}
+                  <div className='row py-2 formRow'>
+                    <div className='col'>
+                      <input
+                        type='text'
+                        name='subject'
+                        className='form-control formInput'
+                        placeholder='Subject'
+                      ></input>
+                    </div>
+                  </div>
+                  {/* Row 4 of form */}
+                  <div className='row py-2 formRow'>
+                    <div className='col'>
+                      <textarea
+                        rows={3}
+                        name='message'
+                        className='form-control formInput'
+                        placeholder='Message'
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className='row py-2 formRow'>
+                    <div className='col text-left'>
+                      <button className='btn btn-primary' type='submit'>
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,21 @@
 export function Footer() {
   return (
     <footer>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <p>Copyright 2024</p>
     </footer>
   )
