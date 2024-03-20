d3f8560d158cdb7b0a206f8f88065082a66e4759
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-20 17:25:32 -0500

    resume link opens in new tab

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,10 +2,7 @@ export function Experience() {
   return (
     <div>
       <h2>Experience</h2>
-      <a
-        href="/Behlmann_resume.pdf"
-        download
-      >
+      <a href="/Behlmann_resume.pdf" target="_blank">
         Download Resume
       </a>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ante at quam tristique venenatis. Nunc laoreet quam sit amet odio convallis, id varius nisl condimentum. Donec aliquet nunc purus, vel congue quam accumsan quis. Quisque ut ultricies nisl, nec ultrices ipsum. Nam eget quam quis odio elementum condimentum. Nunc commodo lacus vel viverra faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus sodales eget nisi at eleifend. Pellentesque odio augue, ullamcorper sit amet dolor at, gravida venenatis augue. Nunc ex mi, consequat at urna et, molestie vestibulum nibh. Etiam eu nisl a tortor pretium sagittis non a libero. Nunc gravida semper ante sit amet dignissim.
