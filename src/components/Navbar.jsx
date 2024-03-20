578893c672b527fa7b4d936517a5e57385239421
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-20 13:34:16 -0500

    set up dev and prod links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
-export function Navbar( {currentSection} ) {
+export function Navbar({ currentSection }) {
+  const developmentURL = "http://localhost:5173";
+  const productionURL = "https://adorable-flan-fc8e81.netlify.app";
+
+  const baseURL = process.env.NODE_ENV === "development" ? developmentURL : productionURL;
+
   return (
     <div>
-      <h5 className={currentSection === "Projects" ? "underline" : ""} >
-        <a className="navlink" href="http://localhost:5173/#projectsSection">Projects</a>
+      <h5 className={currentSection === "Projects" ? "underline" : ""}>
+        <a className="navlink" href={`${baseURL}/#projectsSection`}>Projects</a>
       </h5>
-      <h5 className={currentSection === "About" ? "underline" : ""} >
-        <a className="navlink" href="http://localhost:5173/#aboutSection">About</a>
+      <h5 className={currentSection === "About" ? "underline" : ""}>
+        <a className="navlink" href={`${baseURL}/#aboutSection`}>About</a>
       </h5>
-      <h5 className={currentSection === "Experience" ? "underline" : ""} >
-        <a className="navlink" href="http://localhost:5173/#experienceSection">Experience</a>
+      <h5 className={currentSection === "Experience" ? "underline" : ""}>
+        <a className="navlink" href={`${baseURL}/#experienceSection`}>Experience</a>
       </h5>
-      <h5 className={currentSection === "Contact" ? "underline" : ""} >
-        <a className="navlink" href="http://localhost:5173/#contactSection">Contact</a>
+      <h5 className={currentSection === "Contact" ? "underline" : ""}>
+        <a className="navlink" href={`${baseURL}/#contactSection`}>Contact</a>
       </h5>
     </div>
-  )
+  );
 }
