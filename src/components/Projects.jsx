5a9ee4f92ca4d8c17abef1d94c0b8dde779fa590
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-29 12:25:13 -0500

    updated zyzz in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,7 @@ export function Projects() {
         <div className="card border-secondary p-2">
           <a className="link" href="#" target="_blank">Portfolio Site</a>
           <div>
-            <p>This is the site you're on right now! It's built using React for dynamic user interaction and Bootstrap for clean, responsive design. It's deployed on Netlify so that it's easily accessible. Integration with emailjs allows visitors to contact me directly. It offers a simple yet effective platform shows my design preference for simple and obvious websites.</p>
+            <p>This is the site you're on right now! It's built using React for dynamic user interaction and Bootstrap for a clean and responsive design. It's deployed on Netlify so that it's easily accessible. Integration with emailjs allows visitors to contact me directly. There's also some fun css throughout the site. It offers a simple yet effective platform which shows my design preference for simple and easy to use websites.</p>
           </div>
           <div className="row row-cols-auto g-2">
             <div className="col">
@@ -55,17 +55,20 @@ export function Projects() {
         <div className="card border-secondary p-2">
           <a className="link" href="https://github.com/jmbehlmann/zizz-frontend-2" target="_blank">Zyzz</a>
           <div>
-            <p>Portfolio site summary Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ante at quam tristique venenatis. Nunc laoreet quam sit amet odio convallis, id varius nisl condimentum. Donec aliquet nunc purus, vel congue quam</p>
+            <p>Zyzz is a microblogging site designed to provide users with a familiar social media experience. Built using Rails, React, PostgreSQL, and Bootstrap, Zyzz combines powerful backend functionality with a sleek and intuitive frontend design. Users can post buzzes, follow other users and see their buzzes, and comment on other buzzes. With PostgreSQL handling data storage and React ensuring dynamic user interactions, Zyzz offers a seamless and responsive platform for social networking.</p>
           </div>
           <div className="row row-cols-auto g-2">
             <div className="col">
-              <h5><span className="badge badge-pill">Bootstrap</span></h5>
+              <h5><span className="badge badge-pill">Rails</span></h5>
             </div>
             <div className="col">
               <h5><span className="badge badge-pill">React</span></h5>
             </div>
             <div className="col">
-              <h5><span className="badge badge-pill">Rails</span></h5>
+              <h5><span className="badge badge-pill">Bootstrap</span></h5>
+            </div>
+            <div className="col">
+              <h5><span className="badge badge-pill">PostgreSQL</span></h5>
             </div>
           </div>
         </div>
