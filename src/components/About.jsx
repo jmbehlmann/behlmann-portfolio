5960bf04d8735224a5514f21702b017ccb96876a
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-29 14:01:20 -0500

    added mountain image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export function About() {
     <div>
       <h2>About</h2>
         <p>Hey there, I'm James.</p>
-        <p>In a past life, I was all about music. I've been in a (quite unpopular) <a className="link" href="https://archive.org/details/HKNMusic" target="_blank">jam band</a>, written some wild contemporary classical <a className="link" href="https://www.youtube.com/watch?v=s69Oi1XfHNs" target="_blank">compositions</a>, and been first chair double bassist in an <span className="pic">orchestra</span>. But then life threw me a curveball, and I found myself in the world of mental health counseling.</p>
+        <p>In a past life, I was all about music. I've been in a (quite unpopular) <a className="link" href="https://archive.org/details/HKNMusic" target="_blank">jam band</a>, written some wild contemporary classical <a className="link" href="https://www.youtube.com/watch?v=s69Oi1XfHNs" target="_blank">compositions</a>, and been first chair double bassist in an <span className="orch-pic">orchestra</span>. But then life threw me a curveball, and I found myself in the world of mental health counseling.</p>
         <p>Helping others navigate life's ups and downs became my passion. It was an extremely rewarding experience.</p>
         <p>Now, I've added a new chapter to my story – web development! I recently wrapped up my journey at Actualize Coding Bootcamp, where I learned to use many technologies, including Rails, React, databases, and Bootstrap.</p>
-        <p>In addition to programming, I like to cook overly elaborate meals for 2, hike up mountains, and <a className="link" href="https://www.youtube.com/watch?v=eTcR_CqYJYg" target="_blank">cover</a> obscure electronic music on hardware. </p>
+        <p>In addition to programming, I like to cook overly elaborate meals for 2, hike up <span className="mtn-pic">moountains</span>, and <a className="link" href="https://www.youtube.com/watch?v=eTcR_CqYJYg" target="_blank">cover</a> obscure electronic music on hardware. </p>
         <p>So here I am, blending creativity, connection, and problem-solving in the digital world. It's a wild ride, and I'm loving every minute of it! Let's build something amazing together!
       </p>
     </div>
