7bfe15674257c36f3b0ef770f0f9bd03b7d75466
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-29 10:35:27 -0500

    add react hooks to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,23 @@
+import { useForm } from 'react-hook-form';
+
 export function Contact() {
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const { name, email, subject, message } = data;
+
+    console.log('Name: ', name);
+    console.log('Email: ', email);
+    console.log('Subject: ', subject);
+    console.log('Message: ', message);
+  };
+
   return (
     <div>
       <h2>Contact</h2>
@@ -7,16 +26,24 @@ export function Contact() {
           <div className='row'>
             <div className='col-12'>
               <div className='contactForm'>
-                <form id='contact-form' noValidate>
+                <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
                   {/* Row 1 of form */}
                   <div className='row py-2 formRow'>
                     <div className='col'>
                       <input
                         type='text'
                         name='name'
+                        {...register('name', {
+                          required: { value: true, message: 'Please enter your name' },
+                          maxLength: {
+                            value: 30,
+                            message: 'Please use 30 characters or less'
+                          }
+                        })}
                         className='form-control formInput'
                         placeholder='Name'
                       ></input>
+                      {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
                     </div>
                   </div>
                   {/* Row 2 of form */}
@@ -25,9 +52,16 @@ export function Contact() {
                       <input
                         type='email'
                         name='email'
+                        {...register('email', {
+                          required: true,
+                          pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                        })}
                         className='form-control formInput'
                         placeholder='Email address'
                       ></input>
+                      {errors.email && (
+                        <span className='errorMessage'>Please enter a valid email address</span>
+                      )}
                     </div>
                   </div>
                   {/* Row 3 of form */}
@@ -36,20 +70,34 @@ export function Contact() {
                       <input
                         type='text'
                         name='subject'
+                        {...register('subject', {
+                          required: { value: true, message: 'Please enter a subject' },
+                          maxLength: {
+                            value: 75,
+                            message: 'Subject cannot exceed 75 characters'
+                          }
+                        })}
                         className='form-control formInput'
                         placeholder='Subject'
                       ></input>
+                      {errors.subject && (
+                        <span className='errorMessage'>{errors.subject.message}</span>
+                      )}
                     </div>
                   </div>
                   {/* Row 4 of form */}
                   <div className='row py-2 formRow'>
                     <div className='col'>
                       <textarea
-                        rows={3}
+                        rows={5}
                         name='message'
+                        {...register('message', {
+                          required: true
+                        })}
                         className='form-control formInput'
                         placeholder='Message'
                       ></textarea>
+                      {errors.message && <span className='errorMessage'>Please enter a message</span>}
                     </div>
                   </div>
                   <div className='row py-2 formRow'>
