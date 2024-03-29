2fcff8fc2450242d943c2b9f3b471ab1debfc6d3
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-03-29 12:52:49 -0500

    updated about me

--- src/Content.jsx
+++ src/Content.jsx
@@ -40,33 +40,33 @@ export function Content() {
   return (
     <main>
       <div className="container-xxl custom-container">
-        <div className="row p-2 border">
-          <div className="col-lg-5 h-100 one p-5 border">
-            <div className="row p-2 h-25 border d-flex flex-column justify-content-center">
+        <div className="row p-2 ">
+          <div className="col-lg-5 h-100 one p-5 ">
+            <div className="row p-2 h-25  d-flex flex-column justify-content-center">
               <Header />
             </div>
-            <div className="row p-2  h-50 border d-flex flex-column justify-content-center">
+            <div className="row p-2  h-50  d-flex flex-column justify-content-center">
               <Navbar currentSection={currentSection}/>
             </div>
-            <div className="row h-25 p-2 border d-flex justify-content-center">
+            <div className="row h-25 p-2  d-flex justify-content-center">
               <Links />
             </div>
           </div>
-          <div className="col-lg-6 offset-lg-6 two p-5 border">
-            <div>
-              <div className="row border" id="aboutSection">
+          <div className="col-lg-6 offset-lg-6 two p-5 ">
+            <div className="mt-5">
+              <div className="row " id="aboutSection">
                 <About />
               </div>
-              <div className="row border mt-5" id="projectsSection">
+              <div className="row  mt-5" id="projectsSection">
                 <Projects />
               </div>
-              {/* <div className="row border mt-5" id="experienceSection">
+              {/* <div className="row  mt-5" id="experienceSection">
                 <Experience />
               </div> */}
-              <div className="row border mt-5" id="contactSection">
+              <div className="row  mt-5" id="contactSection">
                 <Contact />
               </div>
-              <div className="row border mt-5">
+              <div className="row  mt-5">
                 <Footer />
               </div>
             </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,12 @@ export function About() {
   return (
     <div>
       <h2>About</h2>
-        <p>Hey there, I'm James.<br></br><br></br>In a past life, I was all about music. I've been in a (quite unpopular) jam band, written some wild contemporary classical compositions, and been first chair double bassist in an orchestra. But then life threw me a curveball, and I found myself in the world of mental health counseling.<br></br><br></br>Helping others navigate life's ups and downs became my passion. It was an extremely rewarding experience.<br></br><br></br>Now, I've added a new chapter to my story – web development! I recently wrapped up my journey at Actualize Coding Bootcamp, where I learned to use many technologies, including Rails, React, databases, and Bootstrap.<br></br><br></br>So here I am, blending creativity, connection, and problem-solving in the digital world. It's a wild ride, and I'm loving every minute of it! Let's build something amazing together!
+        <p>Hey there, I'm James.</p>
+        <p>In a past life, I was all about music. I've been in a (quite unpopular) jam band, written some wild contemporary classical compositions, and been first chair double bassist in an orchestra. But then life threw me a curveball, and I found myself in the world of mental health counseling.</p>
+        <p>Helping others navigate life's ups and downs became my passion. It was an extremely rewarding experience.</p>
+        <p>Now, I've added a new chapter to my story – web development! I recently wrapped up my journey at Actualize Coding Bootcamp, where I learned to use many technologies, including Rails, React, databases, and Bootstrap.</p>
+        <p>In addition to programming, I like to cook overly elaborate meals for 2, hike up mountains, and cover obscure electronic music on hardware. </p>
+        <p>So here I am, blending creativity, connection, and problem-solving in the digital world. It's a wild ride, and I'm loving every minute of it! Let's build something amazing together!
       </p>
     </div>
   )
