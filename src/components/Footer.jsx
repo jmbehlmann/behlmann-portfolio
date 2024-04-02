fa9d1849ca20ededa0aa8c144277c3a8f400a748
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-04-02 12:54:50 -0500

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,6 @@ export function Footer() {
       <br></br>
       <br></br>
       <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
       <p>This site was made using react, bootstrap, netlify</p>
       <p>Copyright 2024</p>
     </footer>
